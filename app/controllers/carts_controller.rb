687a2b76c05b8559ca5796814ef4f6da60c8ea15
rordev456 <devidev45@proton.me>
2020-04-15 16:42:05 -0700

    Added new cart security

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -10,7 +10,19 @@ class CartsController < ApplicationController
   # GET /carts/1
   # GET /carts/1.json
   def show
-  end
+    begin
+      @cart = Cart.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      logger.error "Attempt to access invalid cart #{params[:id]}"
+      puts "dawg wtf tho"
+      redirect_to store_url, notice: 'Invalid cart'
+    else
+      respond_to do |format|
+        format.html show.html.erb
+        format.json {render json: @cart}
+      end
+    end
+
 
   # GET /carts/new
   def new
--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,6 +1,6 @@
 class LineItem < ApplicationRecord
 
-  belongs_to :product
+  belongs_to :product, optional: true
   belongs_to :cart
 
   attr_accessor :cart_id, :product_id
